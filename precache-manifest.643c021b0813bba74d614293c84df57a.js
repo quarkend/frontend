a6244f8dd168df02fab54646d85e83a8664cc427
quarkend <39971072+quarkend@users.noreply.github.com>
2022-05-23 15:22:32 +0200

    Updates

---
+++ precache-manifest.643c021b0813bba74d614293c84df57a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "098c708679db11b26af1",
+    "url": "/static/css/main.35266746.chunk.css"
+  },
+  {
+    "revision": "098c708679db11b26af1",
+    "url": "/static/js/main.098c7086.chunk.js"
+  },
+  {
+    "revision": "358b3101318004d0abe9",
+    "url": "/static/js/1.358b3101.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8de86ee97c724c0298600c16681af406",
+    "url": "/index.html"
+  }
+];
--- src/pages/logout/Logout.js
+++
@@ -1,5 +0,0 @@
-function Logout() {
-    document.cookie = 'token=; expires=Thu, 01 Jan 1970 00:00:02 GMT';
-    return document.location.href = '/';
-}
-export default Logout;

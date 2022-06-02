d8000948ab444624428e25faabbf37836c602d9d
quarkend <39971072+quarkend@users.noreply.github.com>
2022-06-02 11:36:23 +0200

    docs

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import {
   Switch,
   Route,
   Redirect,
+  HashRouter,
 } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
-import Profile from './pages/profile/Profile';
+import Profile from "./pages/profile/Profile";
 import Topbar from "./components/topbar/Topbar";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
-import Admin from './pages/profile/Admin';
-import DeleteUser from './pages/profile/DeleteUser';
+import Admin from "./pages/profile/Admin";
+import DeleteUser from "./pages/profile/DeleteUser";
 import UpdateProfilePhoto from "./pages/profile/UpdateProfilePhoto";
 export const AuthContext = React.createContext();
 
@@ -33,14 +34,14 @@ const reducer = (state, action) => {
         isAdmin: true,
         isAuthenticated: true,
         user: action.payload.user,
-        token: action.payload.token
+        token: action.payload.token,
       };
     case "X":
       localStorage.clear();
       return {
         ...state,
         isAuthenticated: false,
-        user: null
+        user: null,
       };
     default:
       return state;
@@ -49,50 +50,54 @@ const reducer = (state, action) => {
 function App() {
   const [state, dispatch] = React.useReducer(reducer, initialState);
   React.useEffect(() => {
-    const user = JSON.parse(localStorage.getItem('user') || null)
-    const token = JSON.parse(localStorage.getItem('token') || null)
+    const user = JSON.parse(localStorage.getItem("user") || null);
+    const token = JSON.parse(localStorage.getItem("token") || null);
     if (user && token) {
       dispatch({
-        type: 'LOGIN',
+        type: "LOGIN",
         payload: {
           user,
-          token
-        }
-      })
+          token,
+        },
+      });
     }
-  }, [])
+  }, []);
   return (
     <Router>
-      <AuthContext.Provider
-        value={{
-          state,
-          dispatch
-        }}
-      >
-        <Topbar />
-        {/* <div className="App">{!state.isAuthenticated ? <Login/> : <Home/>}</div> */}
-        <Switch>
-          <Route exact path="/">
-            {state.isAuthenticated ? <Home /> : <Register />}
-          </Route>
-          <Route path="/login">{state.isAuthenticated ? <Redirect to="/" /> : <Login />}</Route>
-          <Route path="/register">
-            {state.isAuthenticated ? <Redirect to="/" /> : <Register />}
-          </Route>
-          <Route path="/profile/:username">
-            <Profile />
-          </Route>
-          <Route path="/admin/:id">
-            {state.isAuthenticated ? <Admin /> : <Redirect to="/" />}
-          </Route>
-          <Route path="/deleteuser/:id">
-            <DeleteUser />
-          </Route>
-          <Route path="/updateprofilephoto">
-            <UpdateProfilePhoto />
-          </Route>
-        </Switch>
-      </AuthContext.Provider>
+      <HashRouter basename="/">
+        <AuthContext.Provider
+          value={{
+            state,
+            dispatch,
+          }}
+        >
+          <Topbar />
+          {/* <div className="App">{!state.isAuthenticated ? <Login/> : <Home/>}</div> */}
+          <Switch>
+            <Route exact path="/">
+              {state.isAuthenticated ? <Home /> : <Register />}
+            </Route>
+            <Route path="/login">
+              {state.isAuthenticated ? <Redirect to="/" /> : <Login />}
+            </Route>
+            <Route path="/register">
+              {state.isAuthenticated ? <Redirect to="/" /> : <Register />}
+            </Route>
+            <Route path="/profile/:username">
+              <Profile />
+            </Route>
+            <Route path="/admin/:id">
+              {state.isAuthenticated ? <Admin /> : <Redirect to="/" />}
+            </Route>
+            <Route path="/deleteuser/:id">
+              <DeleteUser />
+            </Route>
+            <Route path="/updateprofilephoto">
+              <UpdateProfilePhoto />
+            </Route>
+          </Switch>
+        </AuthContext.Provider>
+      </HashRouter>
     </Router>
   );
 }
